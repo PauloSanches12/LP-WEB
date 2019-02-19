e2686fc5ec6671db470e875325871c5f4b99cce1
Paulo Sergio <PauloSanches>
2019-02-19 00:00:21 -0300

    lpweb

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'LPWEB';
+  semestre = '2019/1';
+}
